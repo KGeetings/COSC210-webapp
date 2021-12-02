d71f60cebfcbd83cf9d091171fe36fa865dac456
Kenyon <kengeet56@gmail.com>
2021-12-01 22:34:38 -0600

    finished Project 4, except for E.C.

--- www/StudySpaces/Project4/index.php
+++ www/StudySpaces/Project4/index.php
@@ -15,6 +15,7 @@
  <body>
 	 <h1>Central College Study Spaces</h1>
 	<?php
+	// Container 1 is the heading above
 	$hostname = "db";
 	$username = "root";
 	$password = "password";
@@ -25,7 +26,7 @@
 		echo "<h2>For which building do you wish to search?</h2>\n";
 	
 		$buildings = array();
-		// get the buidling names
+		// get the buidling names - Container 2
 		$query = "select name from buildings ";
 		$result = $connect->query($query);
 		if (!$result) die ($connect->error);
@@ -47,6 +48,7 @@
 		echo "</form>\n";
 	}
 	else if (!isset($_POST['location'])){
+		// Container 2
 		echo "<h2>For which location do you wish to search?</h2>\n";
 		$bldg = $_POST['building'];
 		$query = "select id, description from locations ".
@@ -68,6 +70,7 @@
 		echo "</form>\n";
 		}
 	else { 
+		// Prints the location and once finished prints the location and building Container 2
 		$bldg = $_POST['building'];
 		$loc = $_POST['location'];
 		$query1 = "select description from locations where buildingid = (select id from buildings where name = '$bldg') AND id = '$loc'";
@@ -94,6 +97,7 @@
 		$newcolumn1 = strstr($result2img[1], '/Images');
 		$newcolumn2 = strstr($result2img[2], '/Images');
 		
+		// Prints the images for Container 3
 		print <<<IMAGES
 			<div class="images">
 				<img src="$newcolumn0" id="image0">
@@ -102,11 +106,11 @@
 			</div>
 		IMAGES;
 		
+		// Create Resources and Spaces data Container 4
 		echo "<div class = 'res_space'>";
 		
 		echo "<table><tr><td>";
-		echo "<h3>Available Resources: </h3></td>";
-		echo "<h4>";
+		echo "Available Resources:</td>";
 		$query3 = "select description from resources where id in (select resource_id from loc_res where loc_id = '$loc' AND building_id = (select id from buildings where name = '$bldg'))";
 		$result3 = $connect->query($query3);
 		if (!$result3) die ($connect->error);
@@ -115,11 +119,10 @@
 		for ($r = 0; $r < $rows3; ++$r){
 			$result3->data_seek($r);
 			$columns3 = $result3->fetch_array(MYSQLI_NUM);
-			echo "$columns3[0]\n";
+			echo "$columns3[0], ";
 			}
-		echo "</td></tr></h4><tr><td>";
-		echo "<h3>Seating: </h3>";
-		echo "<h4>";
+		echo "</td></tr><tr><td>";
+		echo "Seating:</td><td>";
 		$query4 = "select seating from spaces where location_id = '$loc' AND building_id = (select id from buildings where name = '$bldg')";
 		$result4 = $connect->query($query4);
 		if (!$result4) die ($connect->error);
@@ -129,9 +132,8 @@
 			$columns4 = $result4->fetch_array(MYSQLI_NUM);
 			echo "$columns4[0]";
 			}
-		echo "</td></tr></h4><tr><td>";
-		echo "<h3>Computers: </h3>";
-		echo "<h4>";
+		echo "</td></tr><tr><td>";
+		echo "Computers:</td><td>";
 		$query5 = "select computers from spaces where location_id = '$loc' AND building_id = (select id from buildings where name = '$bldg')";
 		$result5 = $connect->query($query5);
 		if (!$result5) die ($connect->error);
@@ -141,9 +143,8 @@
 			$columns5 = $result5->fetch_array(MYSQLI_NUM);
 			echo "$columns5[0]";
 			}
-		echo "</td></tr></h4><tr><td>";
-		echo "<h3>Food: </h3>";
-		echo "<h4>";
+		echo "</td></tr><tr><td>";
+		echo "Food:</td><td>";
 		$query6 = "select food from spaces where location_id = '$loc' AND building_id = (select id from buildings where name = '$bldg')";
 		$result6 = $connect->query($query6);
 		if (!$result6) die ($connect->error);
@@ -153,9 +154,8 @@
 			$columns6 = $result6->fetch_array(MYSQLI_NUM);
 			echo "$columns6[0]";
 			}
-		echo "</td></tr></h4><tr><td>";
-		echo "<h3>Noise level: </h3>";
-		echo "<h4>";
+		echo "</td></tr><tr><td>";
+		echo "Noise level:</td><td>";
 		$query7 = "select noise from spaces where location_id = '$loc' AND building_id = (select id from buildings where name = '$bldg')";
 		$result7 = $connect->query($query7);
 		if (!$result7) die ($connect->error);
@@ -165,9 +165,8 @@
 			$columns7 = $result7->fetch_array(MYSQLI_NUM);
 			echo "$columns7[0]";
 			}
-		echo "</td></tr></h4><tr><td>";
-		echo "<h3>Lighting: </h3>";
-		echo "<h4>";
+		echo "</td></tr><tr><td>";
+		echo "Lighting:</td><td>";
 		$query8 = "select lighting from spaces where location_id = '$loc' AND building_id = (select id from buildings where name = '$bldg')";
 		$result8 = $connect->query($query8);
 		if (!$result8) die ($connect->error);
@@ -177,9 +176,8 @@
 			$columns8 = $result8->fetch_array(MYSQLI_NUM);
 			echo "$columns8[0]";
 			}
-		echo "</td></tr></h4><tr><td>";
-		echo "<h3>Rating: </h3>";
-		echo "<h4>";
+		echo "</td></tr><tr><td>";
+		echo "Rating:</td><td>";
 		$query9 = "select rating from spaces where location_id = '$loc' AND building_id = (select id from buildings where name = '$bldg')";
 		$result9 = $connect->query($query9);
 		if (!$result9) die ($connect->error);
@@ -189,8 +187,18 @@
 			$columns9 = $result9->fetch_array(MYSQLI_NUM);
 			echo "$columns9[0]";
 			}
-		echo "</td></tr></h4></table>";
+		echo "</td></tr></table>";
 		echo "</div>";
+
+		// Container 5 - Footer with my design credits
+		print <<<FOOTER
+			<div class="footer">
+				<p>
+					This page was designed and developed by Kenyon Geetings, a student at Central College
+					in the Department of Computer Science for COSC-210: Database and the Web.
+				</p>
+			</div>
+		FOOTER;
 	}
 	?>
  </body>
